a470e8cddef2b1cba415d86b5513cc3c07c6feb7
Ulukbek Nusubaliev <ulukbeknusubaliev@Ulukbeks-MacBook-Air.local>
2019-04-04 21:04:05 -0500

    created array of names and list of food

---
+++ js/app.js
@@ -0,0 +1,111 @@
+
+
+
+//created names and foods 
+
+let names = ['Greg','Bill', 'Anill', 'Porter', 'Thiago', 'Lana', 'Wes',
+'Justin', 'Taniya', 'Zack'];
+
+let foods = ['pizza', 'taco', 'burger', 'apple', 'peanuts', 'watermelon', 'bread', 'avacado',
+'cake', 'fish', 'carrot', 'chicken', 'meat'];
+
+
+
+const game = {
+
+	customers:[]
+	
+	cash:50,
+
+	createClient: function(){
+		s
+	},
+
+	orderFood: function(){
+		for(let i=0; i < this.client.length; i++){
+		console.log(`I would like to order some ${this.client.food}`)
+		}
+	}
+}
+
+
+
+
+// const customers = [
+
+// {name: 'Greg',
+//  food:['sushi', 'burger', 'fish', 'carrot']},
+
+// {name: 'Bill',
+//  food:['apple', 'pizza', 'bread', 'soda']},
+
+
+// {name: 'Anill',
+//  food:['soda', 'bread', 'avocado', 'meat']},
+
+
+// {name: 'Porter',
+//  food:['peanuts', 'apple', 'burger', 'ice-cream']},
+
+
+// {name: 'Thiago',
+//  food:['taco', 'pizza', 'watermelon', 'soda']},
+
+
+// {name: 'Lana',
+//  food:['avocado', 'burger', 'ice-cream', 'bread']},
+
+
+// {name: 'Wes',
+//  food:['burger', 'watermelon', 'fish', 'carrot']},
+
+
+
+// {name: 'Porter',
+//  food:['watermelon', 'chicken', 'ice-cream', 'cake']},
+
+
+
+// {name: 'Porter',
+//  food:['fish', 'taco', 'pizza', 'soda']},
+
+
+
+// {name: 'Justin',
+//  food:['peanuts', 'meat', 'cake', 'burger']},
+
+// ]
+
+//list of food u have to guess
+
+//1. ice cream
+//2. pizza
+//3. taco
+//4. burger
+//5. apple
+//6. peanuts
+//7. watermelon
+//8. bread
+//9. avacado
+//10. cake
+//11. fish
+//12. carrot
+//13. chicken
+//14. meat
+
+
+
+
+
+// class Clients{
+// 	constructor(name, cash, food){
+// 		this.name = name;
+// 		this.cash = cash;
+// 		this.food = food;
+// 	}
+// 	orderFood(){
+// 		for(let i=0; i<this.food.length; i++){
+// 		console.log(`Hey i want some ${food[i]}`);
+// 	}
+//   }
+// }
